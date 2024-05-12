8f200c9f6a21985efe4089ddae466a4fc4789640
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-05-12 13:30:10 +0800

    Test head size tail and at linkedLists operations
    todo checkLists: pop [x] -> rm the last element from the lists
      contains [x] -> returns true if it contains a value from the lists
      find [x] returns the index of the node containing value, or null if
    not found
      toString[x] -> returns (value) -> (value) -> null

--- linkedLists.js
+++ linkedLists.js
@@ -36,5 +36,61 @@ const linkedLists = () => {
     return sample;
   };
 
-  return { append, prepend };
+  const size = (node) => {
+    let tmp = node;
+    let len = node ? 1 : 0;
+
+    while (tmp.next !== null) {
+      tmp = tmp.next;
+      len++;
+    }
+
+    return {
+      len,
+    };
+  };
+
+  const head = (nodes) => {
+    let tmp = nodes;
+    tmp.next = null;
+
+    return tmp;
+  };
+
+  const tail = (nodes) => {
+    let tmp = nodes;
+    let end = {};
+
+    while (tmp.next !== null) {
+      tmp = tmp.next;
+    }
+
+    if (tmp.next === null) {
+      end = tmp;
+    }
+
+    return end;
+  };
+
+  const at = (nodes, index, size) => {
+    let tmp = nodes;
+    let given = index;
+
+    if (index > size(tmp).len) {
+      tmp = null;
+      return tmp;
+    }
+
+    for (let i = 0; i < index - 1; i++) {
+      tmp = tmp.next;
+    }
+
+    tmp.next = null;
+
+    return tmp;
+  };
+
+  return { append, prepend, size, head, tail, at };
 };
+
+export default linkedLists;
